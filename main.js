00186eb956a45d5c4aea1f93a27fca001c93064c
g19901 <75403429+g19901@users.noreply.github.com>
2021-01-08 18:18:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText = '今日は寒い寒い冬の日です。 :insertx: は友人のXと一緒に雪の中を歩いていました。Xも :insertx: もとても寒がりだったので文句ばかり言っていました。すると突然雪の中から杖をもったおじさんと弟子Yが現れたのです。おじさんは文句ばかり言っているとバチが当たるぞと言い消えてしまいました。弟子Yは見張っていろと言われたからと二人について歩き始めました。「僕も文句ばかり言っていたんだ」と突然弟子が言いました。「そうしたらおじさんに弟子にされてしまってこの世界に戻って来られなくなってしまったんだ」弟子がそう言ったにも関わらずXはその言葉を信じなかったため文句を言い続け結局おじさんの弟子にさせられ今までいた世界には戻って来られなくなってしまった。';
+let insertX = ['友人X','杖を持ったおじさん','弟子Y'];
+let insertY = ['冬の日','雪','世界'];
+let insertZ = ['雪の中を歩いていた','文句ばかり言っていた','世界に戻って来られなくなった'];
 
 randomize.addEventListener('click', result);
 
@@ -28,10 +28,10 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('Bob',name);
+    newStory = newStory.replace('ボブ',name);
   }
 
-  if(document.getElementById("uk").checked) {
+  if(document.getElementById("jp").checked) {
     const weight = Math.round(300*0.0714286) + ' stone';
     const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
     newStory = newStory.replace('94 fahrenheit',temperature);
